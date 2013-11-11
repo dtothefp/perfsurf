c4fa49aa3981aa90401ac6a4ebaa8898b9d29f09
David Fox Powell <dtothefp@gmail.com>
2013-11-11 14:54:41 -0500

    Add bcrypt Gem and password digest to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131108232244) do
+ActiveRecord::Schema.define(version: 20131111194809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
-    t.text     "email",      null: false
+    t.text     "email",           null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest", null: false
   end
 
 end
