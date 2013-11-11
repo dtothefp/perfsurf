c2759a47f2f396e8894759b5a36845b2645ba016
David Fox Powell <dtothefp@gmail.com>
2013-11-11 17:32:23 -0500

    complete user CRUD, no authentication/authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,37 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to user_path(@user)
     else
-      binding.pry
       render :new
     end
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
     
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+
+    if @user.destroy
+      redirect_to new_user_path
+    else
+      render :edit
+    end
   end
 
   private
