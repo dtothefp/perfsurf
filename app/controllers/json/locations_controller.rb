a6d32b8ac99a6693fb42637f1fb64fce6be19021
David Fox Powell <dtothefp@gmail.com>
2013-11-12 16:52:03 -0500

    successful YQL integration for MSW api

--- app/controllers/json/locations_controller.rb
+++ app/controllers/json/locations_controller.rb
@@ -1,12 +1,16 @@
 class Json::LocationsController < ApplicationController
 
+  before_action :admin?
+
   def index
     @locations = Location.all
     render json: @locations
   end
 
   def show
-    
+    @location = Location.find(params[:id])
+
+    render json: @location
   end
 
 end
