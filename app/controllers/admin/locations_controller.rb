e1baf0be5a5d40862ed7160c89b763cc2a1d890f
David Fox Powell <dtothefp@gmail.com>
2013-11-12 14:47:00 -0500

    Added location creation admin functionality

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -1,8 +1,29 @@
 ## ADMIN LOCATIONS CONTROLLER ##
 class Admin::LocationsController < ApplicationController
 
+  before_action :admin?
+
   def index
-    
+    @locations = Location.all
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(admin_location_params)
+    if @location.save
+      redirect_to admin_locations_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def admin_location_params
+    params.require(:location).permit(:name, :latitude, :longitude, :msw_id)
   end
 
 end
