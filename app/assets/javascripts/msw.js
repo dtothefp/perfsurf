5d509f7e39c2bc9bb9016c34346276f3c4e2d037
David Fox Powell <dtothefp@gmail.com>
2013-11-12 19:49:19 -0500

    worked on MSW ajax, not working consitenly, moving to HTTParty

---
+++ app/assets/javascripts/msw.js
@@ -0,0 +1,41 @@
+function createImageElements(responseObj, attribute){
+  // console.log(responseObj);
+  var parsedResponse = JSON.parse(responseObj);
+  console.log(parsedResponse);
+  $.each(parsedResponse, function(key, singleObj){
+    var imgElement = $('<img>').attr('src', singleObj.charts[attribute]);
+    $('#msw-data').append($('<div>').addClass('ms-img-placeholder').append(imgElement));
+  });
+}
+
+function getSpotData(id, callback, attr){
+  var site = 'http://magicseaweed.com/api/pGQx9a7IXwVYs3lIcQ8RV5E0y74Xmg9Y/forecast/?spot_id=' + id;
+  var yql = 'http://query.yahooapis.com/v1/public/yql?q=' +
+             encodeURIComponent('select * from html where url="' + site + '"') +
+            '&format=json';
+  console.log("AJAX REQ: " + yql);
+
+  $.ajax({
+        url: yql,
+        type: "GET",
+        // crossDomain: true,
+        // xhrFields: {
+        //   withCredentials: true
+        // },
+        dataType: "json"
+        // success: function() { alert("Success"); },
+        // error: function() { alert('Failed!'); },
+        // beforeSend: setHeader
+      }).done(function(data){
+        if (data.query.results === null) {
+          console.log("results null");
+          console.log(data); 
+        } else{
+          var responseData = data.query.results.body.p;
+          callback(responseData, attr);
+        }
+      }).fail(function(jqXHR, textStatus, errorThrown){
+        alert(errorThrown);
+        console.log(textStatus);
+  });
+}
