7aa29c1ad6ea2c3e5ab5221c26203c8020162380
David Fox Powell <dtothefp@gmail.com>
2013-11-14 12:20:08 -0500

    annimation for locations charts

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -13,6 +13,8 @@ class LocationsController < ApplicationController
     @location = Location.find(params[:id])
     @user = User.find(session[:user_id])
     @responses = Response.new( HTTParty.get("http://magicseaweed.com/api/pGQx9a7IXwVYs3lIcQ8RV5E0y74Xmg9Y/forecast/?spot_id=" + @location.msw_id.to_s ) )
+    @msw = @responses.get_http_obj
+    @chart_array = @responses.get_chart_data.to_json
     @stats = @responses.timestamp_to_object
   end
 
--- app/models/response.rb
+++ app/models/response.rb
@@ -7,12 +7,26 @@ class Response
   def get_http_obj
     @http_obj
   end
+
+  def get_chart_data
+    chart_array = []
+    
+    self.get_http_obj.each do |response|
+      individual_chart_data = []
+      individual_chart_data << Time.at(response["timestamp"]).strftime("%A, %b %e")
+      individual_chart_data << Time.at(response["timestamp"]).strftime("%l%p")
+      individual_chart_data << response["charts"]["swell"]
+      individual_chart_data << response["charts"]["wind"]
+      chart_array << individual_chart_data
+    end
+    chart_array
+  end
   
   def timestamp_to_object
     full_stats = {}
     prev_day = ""
     time_data = {}
-    @http_obj.each do |response|
+    self.get_http_obj.each do |response|
       day = Time.at(response["timestamp"]).strftime("%A, %b %e")
       hour = Time.at(response["timestamp"]).strftime("%l%p")
 
