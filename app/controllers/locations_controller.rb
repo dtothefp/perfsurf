5598f203d4fe5b008eb5f167b81772ad776dae50
David Fox Powell <dtothefp@gmail.com>
2013-11-13 19:35:13 -0500

    Created grids for stats on individual locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -14,6 +14,7 @@ class LocationsController < ApplicationController
     @user = User.find(session[:user_id])
     @responses = Response.new( HTTParty.get("http://magicseaweed.com/api/pGQx9a7IXwVYs3lIcQ8RV5E0y74Xmg9Y/forecast/?spot_id=" + @location.msw_id.to_s ) )
     @stats = @responses.timestamp_to_object
+    binding.pry
   end
 
 end
