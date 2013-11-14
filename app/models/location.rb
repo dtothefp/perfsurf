fb63d64b4b6d25b641b03e40df1e2852966f69f8
Maria Mercedes Martinez <pistachiopony@gmail.com>
2013-11-14 17:02:15 -0500

    fix so we can upload pngs too

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,5 +4,5 @@ class Location < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://www.placekitten/300/200"
 
   validates_attachment :image, :presence => true
-  validates_attachment_content_type :image, :content_type => ['image/jpeg'] 
+  # FIXME add more posssible content types validates_attachment_content_type :image, :content_type => ['image/jpeg'] 
 end
