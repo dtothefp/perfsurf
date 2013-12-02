1cb31789b1b80df21d9e0e7ba827096a7c2e94d8
David Fox Powell <dtothefp@gmail.com>
2013-12-02 18:27:30 -0500

    Push for Maria to pull

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -22,17 +22,19 @@ class LocationsController < ApplicationController
 
   def show
     @location = Location.find(params[:id])
-    @user = User.find(session[:user_id])
+    if logged_in?
+      @user = User.find(session[:user_id])
+    end
     @responses = Response.new( HTTParty.get("http://magicseaweed.com/api/pGQx9a7IXwVYs3lIcQ8RV5E0y74Xmg9Y/forecast/?spot_id=" + @location.msw_id.to_s ) )
     @msw = @responses.get_http_obj
     @chart_array = @responses.get_chart_data.to_json
     @stats = @responses.timestamp_to_object
     # binding.pry
-    # if logged_in?
-    #   render :show
-    # else
-    #   redirect_to new_session_path
-    # end
+    if logged_in?
+      render :show
+    else
+      redirect_to new_session_path
+    end
   end
 
 end
